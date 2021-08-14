4ea89cb7cf92683e5ca3bfa5f93cc667f3649759
Christoph van der Fecht <c.vanderfecht@vdsoft.at>
2021-08-14 18:38:06 +0200

    refactor(test) add tests

---
+++ test/VDsoft.Cqrs.DependencyInjectionExtension.Test/MicrosoftDiTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDsoft.Cqrs.Services;
+using Xunit;
+
+namespace VDsoft.Cqrs.DependencyInjectionExtension.Test
+{
+    public class MicrosoftDiTests
+    {
+        [Fact]
+        public void UseCqrs_CommandAndArgumentFromTestAssembly_CommandCanBeRequestedFromDi()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            var value = "Test Value";
+            var testArguments = new TestArgument(value);
+
+            var dispatcher = new DefaultActionDispatcher(services.BuildServiceProvider());
+
+            // Act
+            services.UseCqrs(typeof(TestCommand).Assembly);
+
+            // Assert
+            var provider = services.BuildServiceProvider();
+            dispatcher.ExecuteAsync(testArguments);
+            Assert.NotNull(provider.GetService(typeof(TestArgument)));
+        }
+    }
+}
---
+++ test/VDsoft.Cqrs.DependencyInjectionExtension.Test/TestCqrs.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using VDsoft.Cqrs.Command;
+using VDsoft.Cqrs.Query;
+
+namespace VDsoft.Cqrs.DependencyInjectionExtension.Test
+{
+    internal class TestArgument : IArguments
+    {
+        public TestArgument(string testValue)
+        {
+            this.TestValue = testValue;
+        }
+
+        public string TestValue { get; }
+    }
+
+    internal class TestCommand : ICommand<TestArgument>
+    {
+        public Task ExecuteAsync(TestArgument arguments)
+        {
+            return Task.FromResult(arguments);
+        }
+    }
+
+    internal class TestQuery : IQuery<int>
+    {
+        public TestQuery(int value)
+        {
+            this.Value = value;
+        }
+
+        public int Value { get; }
+    }
+
+    internal class TestQueryHandler : IQueryHandler<TestQuery, int>
+    {
+        public Task<int> ExecuteAsync(TestQuery query)
+        {
+            return Task.FromResult(query.Value);
+        }
+    }
+}
